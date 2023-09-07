cd24420f0b2b8aba3cc2ba802858b9841eade3aa
Atul Sanodiya <127666795+sanodiyaatul@users.noreply.github.com>
2023-09-07 16:42:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+const addBtn = document.querySelector("#addbtn");
+const main = document.querySelector("#main");
+
+addBtn.addEventListener("click", function () {
+    addNote()
+});
+
+const saveNotes =()=>{
+    const notes = document.querySelectorAll(".note textarea");
+    console.log(notes);
+    const data = [];
+    notes.forEach((note)=>{data.push(note.value)})
+        if(data.length === 0){
+            localStorage.removeItem("notes")
+        }else{
+            localStorage.setItem("notes",JSON.stringify(data))
+        }
+}
+
+
+const addNote = (text = "") => {
+    const note = document.createElement("div");
+    note.classList.add("note");
+    note.innerHTML = `
+    <div class="tool">
+        <i class=" save fa-solid fa-floppy-disk"></i>
+        <i class="trash fa-solid fa-trash"></i>
+    </div>
+    <textarea></textarea>
+    `;
+
+    note.querySelector(".trash").addEventListener("click", function () {
+        note.remove()
+    })
+
+    note.querySelector(".save").addEventListener("click",
+        function () {
+            saveNotes()
+        })
+
+    note.querySelector("textarea").addEventListener("focusout", function () { saveNotes() })
+
+
+    main.appendChild(note);
+    saveNotes()
+}
+
